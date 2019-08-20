a2884be9a701736035b83df994d8e6b9dd223452
asamlhk <asa_hp_choi@manulife.com>
2019-08-20 17:02:04 +0800

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,12 @@ export class AppComponent {
   signed = false;
   documents = [
     {
+      name: 'static Form',
+      url: 'https://firebasestorage.googleapis.com/v0/b/storage-5ad5a.appspot.com/o/T19_USD_DBS%20(11-2018)%20with%20PICS%20-%20Acroform%20-%201%20field.PDF?alt=media&token=18d40fb2-3e96-41ff-805a-0e55c03410cb',
+      signed: false,
+      signfieldsmeta:[]
+    },
+    {
       name: 'USD',
       url: 'https://firebasestorage.googleapis.com/v0/b/storage-5ad5a.appspot.com/o/T19_USD_DBS%20(11-2018)%20with%20PICS%20-%20Acroform%20-%201%20field.PDF?alt=media&token=18d40fb2-3e96-41ff-805a-0e55c03410cb',
       signed: false,
